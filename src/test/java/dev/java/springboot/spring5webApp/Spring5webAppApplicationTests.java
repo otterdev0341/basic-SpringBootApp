d9f3095d13bcea5d73c231b2351a5f447d462617
otterdev_0341 <otterdev_0341@outlook.com>
2024-01-08 01:13:16 +0700

    init project

---
+++ src/test/java/dev/java/springboot/spring5webApp/Spring5webAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java.springboot.spring5webApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5webAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
